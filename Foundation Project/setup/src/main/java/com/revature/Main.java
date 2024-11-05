715f56cd6f5233c67f5deaa5974ebb118b18b39d
BatmenzDW <turtleterracotta@gmail.com>
2024-11-05 14:45:59 -0500

    Commit

--- Foundation Project/setup/src/main/java/com/revature/Main.java
+++
@@ -1,8 +0,0 @@
-package com.revature;
-
-public class Main {
-    /*
-        This main method is available if you want to tinker with things, but the Planetarium
-        setup content is located in the Setup class in the test directory
-     */
-}
